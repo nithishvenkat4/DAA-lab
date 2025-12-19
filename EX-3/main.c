359091c9419bb802690c6c9c8187fadfc2076151
nithishvenkat4 <nithishvenkat4507@gmail.com>
2025-12-19 15:12:07 +0530

    Closest Pair Started and Going

--- EX-3/main.c
+++ EX-3/main.c
@@ -1,8 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+struct Point{
+int x;
+int y;};
+
+int main(){
+
+int n=20;
+struct Point points[n];
+struct Point temp;
+srand(time(NULL));
+
+for (int i=0; i<n ; i++){
+    points[i].x = rand() % 100;
+    points[i].y = rand() % 100;
+}
+
+for (int i=0;i<n;i++){printf("%d  %d \n",points[i].x,points[i].y);}
+printf("New \n");
+for(int i=0;i<=n-2;i++){
+    for(int j=0;j<=n-2-i;j++){
+        if(points[j].x>points[j+1].x){
+            temp=points[j];
+            points[j]=points[j+1];
+            points[j]=temp;
+        }
+    }
+}
+for (int i=0;i<n;i++){printf("%d  %d \n",points[i].x,points[i].y);}
+
+
+return 0;
 }
